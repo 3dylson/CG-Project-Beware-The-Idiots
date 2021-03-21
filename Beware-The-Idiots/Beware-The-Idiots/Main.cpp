d9030f829c1a05cf20897edf9d43697b339905a0
Edylson Manuel Gonçalves Frederico <edylson_frederico@outlook.com>
2021-03-21 00:43:37 +0000

    animation test

--- Beware-The-Idiots/Beware-The-Idiots/Main.cpp
+++ Beware-The-Idiots/Beware-The-Idiots/Main.cpp
@@ -3,10 +3,11 @@
 
 void display();
 void reshape(int, int);
+void timer(int);
 
 void init()
 {
-	//range of colors: [0,1]
+	//gama de cores: [0,1]
 	glClearColor(0.0, 0.0, 0.0, 1.0);
 }
 
@@ -14,7 +15,7 @@ void init()
 int main(int argc,char**argv)
 {
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_RGB);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
 
 	glutInitWindowPosition(200, 100);
 	glutInitWindowSize(500, 500);	
@@ -23,33 +24,81 @@ int main(int argc,char**argv)
 
 	glutDisplayFunc(display);
 	glutReshapeFunc(reshape);
+	glutTimerFunc(0,timer,0);
 	init();
 	
 	glutMainLoop();
-	
+
+	return 0;
 }
 
+float x_position = -10.0;
+int state = 1;
+
+/**
+ * \brief Função para exibir tudo
+ */
 void display()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
 	glLoadIdentity();
-	
-	//draw
 
+	//em pixels
+	glPointSize(10.0);
+	//desenhar
+	glBegin(GL_POLYGON);
+
+	glVertex2f(x_position,1.0);
+	glVertex2f(x_position,-1.0);
+	glVertex2f(x_position+2.0,-1.0);
+	glVertex2f(x_position+2.0,1.0);
+
+	//
+	glEnd();
 	
-	glFlush();
+	
+	glutSwapBuffers();
 	
 }
 
+/**
+ * \brief função para remodelar
+ * \param w largura
+ * \param h altura
+ */
 void reshape(const int w, const int h)
 {
-	//viewport
+	//janela de exibição
 	glViewport(0, 0, static_cast<GLsizei>(w), static_cast<GLsizei>(h));
-	//projection
+	//projeção
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 
 	gluOrtho2D(-10, 10, -10, 10);
 	glMatrixMode(GL_MODELVIEW);
 }
+
+void timer(int)
+{
+	//chamar a função display
+	glutPostRedisplay();
+	glutTimerFunc(1000 / 60, timer, 0);
+
+	switch (state)
+	{
+	case 1:
+		if (x_position < 8)
+			x_position += 0.15;
+		else
+			state = -1;
+		break;
+	case -1:
+		if (x_position > -10)
+			x_position -= 0.15;
+		else state = 1;
+		break;
+
+	}
+}
+
 
